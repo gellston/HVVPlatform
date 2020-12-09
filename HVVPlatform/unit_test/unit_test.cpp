35e8ac6fc482119805d3fef7d606206e686c5cd6
bonghoe <wantrnqhdghl@naver.com>
2020-12-09 21:42:45 +0900

    unit_test 프로젝트 추가.

---
+++ HVVPlatform/unit_test/unit_test.cpp
@@ -0,0 +1,39 @@
+﻿
+#define BOOST_TEST_MODULE hvv_script_test
+#define BOOST_TEST_SHOW_PROGRESS
+
+#include <boost/test/included/unit_test.hpp>
+
+#include <object.h>
+#include <primitive_object.h>
+
+
+using namespace hv::v1;
+
+BOOST_AUTO_TEST_SUITE(hvv_script_test)
+
+BOOST_AUTO_TEST_CASE(native_boolean_test)
+{
+	{
+		hv::v1::boolean static_test1("test", false);
+
+		BOOST_CHECK(static_test1.data() == false);
+		BOOST_CHECK(static_test1.name() == "test");
+		BOOST_CHECK(static_test1.type() == "boolean");
+		BOOST_CHECK(static_test1.to_string() == "false");
+	}
+}
+
+BOOST_AUTO_TEST_CASE(native_number_test)
+{
+	{
+		hv::v1::number static_test1("test", 1.5);
+
+		BOOST_CHECK(static_test1.data() == 1.5);
+		BOOST_CHECK(static_test1.name() == "test");
+		BOOST_CHECK(static_test1.type() == "number");
+		BOOST_CHECK(static_test1.to_string() == "1.500000");
+	}
+}
+
+BOOST_AUTO_TEST_SUITE_END()
