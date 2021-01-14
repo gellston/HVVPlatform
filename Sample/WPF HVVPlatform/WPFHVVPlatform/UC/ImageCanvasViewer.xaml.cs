2051e00e814a21c3c229209cb3f3726d5f45e001
gellston <wantrnqhdghl@gmail.com>
2021-01-14 21:50:50 +0900

    1. 이미지 뷰어용 캔버스 제작

---
+++ Sample/WPF HVVPlatform/WPFHVVPlatform/UC/ImageCanvasViewer.xaml.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFHVVPlatform.UC
+{
+    /// <summary>
+    /// ImageCanvasViewer.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ImageCanvasViewer : UserControl, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyRaised(string propertyname)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+
+        public void Set<T>(string _name, ref T _reference, T _value)
+        {
+            if (!Equals(_reference, _value))
+            {
+                _reference = _value;
+                OnPropertyRaised(_name);
+            }
+        }
+
+
+
+        public ImageCanvasViewer()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ImageProperty = DependencyProperty.Register("Image", typeof(BitmapImage), typeof(ImageCanvasViewer), new PropertyMetadata(OnCustomerChangedCallBack));
+        public BitmapImage Image
+        {
+            get
+            {
+                return (BitmapImage)GetValue(ImageProperty);
+            }
+
+            set
+            {
+                SetValue(ImageProperty, value);
+            }
+        }
+
+        private static void OnCustomerChangedCallBack(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            ImageCanvasViewer control = sender as ImageCanvasViewer;
+            if (control != null)
+            {
+                BitmapImage image = e.NewValue as BitmapImage;
+                if (image == null) return;
+
+                
+            }
+        }
+
+        private double _CanvasWidth = 0;
+        public double CanvasWidth
+        {
+            get => _CanvasWidth;
+            set => Set<double>(nameof(CanvasWidth), ref _CanvasWidth, value);
+        }
+
+        private double _CanvasHeight = 0;
+        public double CanvasHeight
+        {
+            get => _CanvasHeight;
+            set => Set<double>(nameof(CanvasHeight), ref _CanvasHeight, value);
+        }
+
+        public static readonly DependencyProperty ZoomProperty = DependencyProperty.Register("Zoom", typeof(double), typeof(ImageCanvasViewer));
+        public double Zoom
+        {
+            get
+            {
+                return (double)GetValue(ZoomProperty);
+            }
+
+            set
+            {
+                SetValue(ZoomProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty ZoomMaxProperty = DependencyProperty.Register("ZoomMax", typeof(double), typeof(ImageCanvasViewer));
+        public double ZoomMax
+        {
+            get
+            {
+                return (double)GetValue(ZoomMaxProperty);
+            }
+
+            set
+            {
+                SetValue(ZoomMaxProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty ZoomMinProperty = DependencyProperty.Register("ZoomMin", typeof(double), typeof(ImageCanvasViewer));
+        public double ZoomMin
+        {
+            get
+            {
+                return (double)GetValue(ZoomMinProperty);
+            }
+
+            set
+            {
+                SetValue(ZoomMinProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty ZoomStepProperty = DependencyProperty.Register("ZoomStep", typeof(double), typeof(ImageCanvasViewer));
+        public double ZoomStep
+        {
+            get
+            {
+                return (double)GetValue(ZoomStepProperty);
+            }
+
+            set
+            {
+                SetValue(ZoomStepProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty TranslationXProperty = DependencyProperty.Register("TranslationX", typeof(double), typeof(ImageCanvasViewer));
+        public double TranslationX
+        {
+            get
+            {
+                return (double)GetValue(TranslationXProperty);
+            }
+
+            set
+            {
+                SetValue(TranslationXProperty, value);
+            }
+        }
+
+
+        public static readonly DependencyProperty TranslationYProperty = DependencyProperty.Register("TranslationY", typeof(double), typeof(ImageCanvasViewer));
+        public double TranslationY
+        {
+            get
+            {
+                return (double)GetValue(TranslationYProperty);
+            }
+
+            set
+            {
+                SetValue(TranslationYProperty, value);
+            }
+        }
+
+        //public static readonly DependencyProperty SelectedItemProperty = DependencyProperty.Register("SelectedItem", typeof(ImageCanvasViewer), typeof(Object), new PropertyMetadata(OnSelectedItemChangedCallBack));
+        //public ClassificationLabelBox SelectedItem
+        //{
+        //    get
+        //    {
+        //        return (ClassificationLabelBox)GetValue(SelectedItemProperty);
+        //    }
+
+        //    set
+        //    {
+        //        SetValue(SelectedItemProperty, value);
+        //    }
+        //}
+        //private static void OnSelectedItemChangedCallBack(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        //{
+        //    BoxLabelControler control = sender as BoxLabelControler;
+        //    if (control != null)
+        //    {
+        //        control.SelectedItem = e.NewValue as ClassificationLabelBox;
+        //    }
+        //}
+    }
+}
